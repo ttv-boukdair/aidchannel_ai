5c0138fb9a81d7c2432c85104223894e690219b7
mohamedbah-byte <clyberr@gmail.com>
2022-06-10 13:28:02 +0200

    up

--- src/code/index.py
+++ src/code/index.py
@@ -37,9 +37,52 @@ def hello():
 def sim_jobs(input : Input):
     req = jsonable_encoder(input)
     text = req['text']
-    ids, dis = get_cos_sim(text, model, index)
+    ids, dis = get_cos_sim(text, model, index, 10)
     res = format_res(ids, dis, umtc)
     return res
+
+@app.get('/normalize-tunisie-utmc')
+def normalize_tunisie_utmc():
+    # connect to db to execute queries
+    cur = conn.cursor()
+    while True:
+        # get offer from db
+        # SELECT id, title FROM data.offer
+        # WHERE data.offer.rtmc_job_designation_id IS NULL
+        # order by data.offer.id desc
+        # limit 1;
+        cur.execute("""SELECT id,title FROM data.offer
+                        WHERE data.offer.config_is_normalized = False AND config_normalization_error = False
+                        order by data.offer.id desc
+                        limit 1;""")
+        offers = cur.fetchall()
+        if len(offers):
+            id, title = offers[0]
+        
+            # normalize offer return id job_designation degre
+
+            ids, dis = get_cos_sim(title, model, index, 1)
+            res = format_res(ids, dis, umtc)
+            if len(res):
+                rtmc_job_designation_id,rtmc_job_designation_title,config_normalized_degre=res[0]
+
+                # update offer(rtmc_job_designation_id, degre, is_normalized = True)
+                #UPDATE data.offer
+	            #SET rtmc_job_designation_id=?,config_is_normalized=?, config_normalized_degre=?
+	            #WHERE data.offer.id = id;
+                cur.execute("""UPDATE data.offer
+                        SET rtmc_job_designation_id={rtmc_job_designation_id},config_is_normalized={True}, config_normalized_degre={config_normalized_degre}
+                        WHERE data.offer.id = {id};""")
+            else:
+                # in case there is a normalization pb update this field config_normalization_error with True
+                cur.execute("""UPDATE data.offer
+                        SET config_normalization_error={True}
+                        WHERE data.offer.id = {id};""")
+            conn.commit()
+
+    return ''
+
+
 
 def getUMTC():
     cur = conn.cursor()
@@ -51,15 +94,15 @@ def vectorizeJobs():
     umtc_jobs_vectors = [model.encode(u[1]) for u in umtc]
     return umtc_jobs_vectors
 
-def get_cos_sim(text, model, index):
+def get_cos_sim(text, model, index, k):
     q = model.encode(text)
-    ids, distances = index.knnQuery(q, k=10)
+    ids, distances = index.knnQuery(q, k=k)
     return ids.tolist(), distances.tolist()
 
 def format_res(ids, dis, umtc):
     formated_res = []
     for i in range(len(ids)):
-        formated_res.append([umtcs[ids[i]][0], umtcs[ids[i]][1], dis[i]])
+        formated_res.append([umtc[ids[i]][0], umtc[ids[i]][1], dis[i]])
     return formated_res
 
 if __name__ == '__main__':
