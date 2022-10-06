de045eb43a86448eda67a8724d8d9ebf617a2233
mohamedbah-byte <clyberr@gmail.com>
2022-10-06 12:00:42 +0200

    up added index skills

--- src/code/index.py
+++ src/code/index.py
@@ -8,6 +8,7 @@ from sentence_transformers import SentenceTransformer
 import nmslib
 from pymongo import MongoClient
 import time
+import spacy
 l = "mongodb://tunisie-tn-jobs:gn!%40Qg%5EFH94MW%5E5Q7me%24@51.77.134.195:29098/tunisie-tn-jobs?authSource=test&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
 DATA_PATH = '/www/data/'
@@ -20,7 +21,9 @@ app.add_middleware(
 )
 class Input(BaseModel):
     text: str
-
+class Input_k(BaseModel):
+    text: str
+    k: int
 
 @app.get('/')
 def hello():
@@ -34,6 +37,34 @@ def sim_jobs(input : Input):
     res = format_res(ids, dis, rtmc)
     return res
 
+@app.post('/sim-skills')
+def sim_skills(input : Input_k):
+    req = jsonable_encoder(input)
+    text = req['text']
+    k = int(req['k'])
+    if sentsLength(nlp(text)) <= 3:
+        res = get_skills_1(text)
+    else:
+        if k:
+            res = get_skills_2(text, df_noise, k)
+        else:
+            res = get_skills_2(text, df_noise)
+    return res
+
+@app.post('/sim-skills-1')
+def sim_skills_1(input : Input_k):
+    req = jsonable_encoder(input)
+    text = req['text']
+    res = get_skills_1(text)
+    return res
+
+@app.post('/sim-skills-2')
+def sim_skills_2(input : Input_k):
+    req = jsonable_encoder(input)
+    text = req['text']
+    k = int(req['k'])
+    res = get_skills_2(text, df_noise, k)
+    return res
 
 @app.get('/normalize-tunisie-rtmc')
 def normalize_tunisie_rtmc():
@@ -79,6 +110,13 @@ def vectorizeJobs():
     rtmc_jobs_vectors = [model.encode(r['name']) for r in rtmc]
     return rtmc_jobs_vectors
 
+def vectorizeSkills(rtmc):
+    rtmc_vectors = []
+    for i in range(len(rtmc)):
+      rtmc_vectors.append(model.encode(rtmc[i]['name']))
+      if i%100 == 0:
+        print(i)
+    return rtmc_vectors
 def get_cos_sim(text, model, index, k):
     q = model.encode(text)
     ids, distances = index.knnQuery(q, k=k)
@@ -94,17 +132,99 @@ def format_res_id(ids, dis, rtmc):
     for i in range(len(ids)):
         formated_res.append([rtmc[ids[i]]['_id'], rtmc[ids[i]]['rtmc_job_id'], rtmc[ids[i]]['name'], dis[i]])
     return formated_res
+
+def getRTMCSkills():
+    rtmc_skills = []
+    cur_rtmc_skills = db.rtmcskills.find({})
+    for skill in cur_rtmc_skills:
+        rtmc_skills.append(skill)
+    return rtmc_skills
+
+def format_res_skills(ids, dis, competences, deg = 0.5):
+    formated_res = []
+    for i in range(len(ids)):
+        if dis[i] >= deg:
+          break
+        formated_res.append([competences[ids[i]]['code_ogr'], competences[ids[i]]['name'], dis[i]])
+    return formated_res
+
+def get_skills_1(text):
+    try:
+        ids, distances = get_cos_sim(text, model, index_competences, 10)
+        return {'text': text, 'competences': format_res_skills(ids, dis, competences)}
+    except Exception as e:
+        print(e)
+        print(j)
+        return {'text': text, 'competences': []}
+
+def get_skills_2(text, df_noise, max_skills = 100):
+  sent_skills = []
+  sents = []
+  doc = nlp(text)
+  for sent in doc.sents:
+    noise = noise_person(sent, df_noise)
+    if noise:
+      print(sent)
+      continue
+    sents.append(sent.text)
+    ids, distances = get_cos_sim(sent.text, model, index_competences, 3)
+    sent_skills = sent_skills + format_res(ids, distances, competences)
+  sent_skills = sorted(sent_skills, key=lambda tup: tup[2])
+  return {'text': text, 'sents': sents,'competences': sent_skills[:max_skills]}
+
+def sentsLength(doc):
+    i = 0
+    for sent in doc.sents:
+        if docLength(sent):
+            i+=1
+    return i
+
+def docLength(doc):
+  i = 0 
+  for token in doc:
+    if token.is_stop or token.tag_ in ['DET', 'ADP', 'CCONJ']:
+      continue
+    i+=1
+  return i
+
+def noise_person(text, df_noise):
+  #if sentence is without useful words
+  if not docLength(text):
+    return True
+  # if verb in sentence is in 1 person singular or plural return true
+  for token in text:
+    if(token.tag_ == 'VERB' and token.morph.get('Person') == ['1']):
+      return True
+    # print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.morph.get('Person'))
+  # if sentence contains org or money it is a noise for skills
+  for ent in text.ents:
+    if ent == 'ORG' or ent == 'MONEY':
+      return True
+  # if noise snippet exists in text return True
+  noise_list = [n.lower() for n in df_noise['Bruit'].tolist()]
+  for noise in noise_list:
+    if noise in text.text.lower():
+      return True
+  return False
+
 if __name__ == '__main__':
     print('loading models ...')
+    df_noise = pd.read_csv('./data/NoiseAction.csv')
     model = SentenceTransformer('dangvantuan/sentence-camembert-large')
     print('connect to db and vectorize jobs ...')
+    nlp = spacy.load('fr_dep_news_trf')
     client = MongoClient(l)
     db = client['tunisie-tn-jobs']
     rtmc = getRTMC()
     rtmc_jobs_vectors = vectorizeJobs()
     print('indexing vectors jobs ...')
+    competences = getRTMCSkills()
+    competences_vectors = vectorizeSkills(competences)
     index = nmslib.init(method='hnsw', space='cosinesimil')
     index.addDataPointBatch(rtmc_jobs_vectors)
     index.createIndex({'post': 2}, print_progress=True)
+    index_competences = nmslib.init(method='hnsw', space='cosinesimil')
+    index_competences.addDataPointBatch(competences_vectors)
+    index_competences.createIndex({'post': 2}, print_progress=True)
     print('API Ready !!!')
     uvicorn.run(app, host='0.0.0.0',port = 80)
